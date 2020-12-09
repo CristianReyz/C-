6976c622eacc8499da79d6bc2127672e345d2782
CristianReyz <cristiandeus.reyes@gmail.com>
2020-12-08 22:29:39 -0600

    incluyendo otro for

--- prefijos.cpp
+++ prefijos.cpp
@@ -18,13 +18,26 @@ int main(){
 			cin>>array[filas][columnas];
 		}
 	}
-	arrayAux[0][1] = array[0][2];
-	arrayAux[1][1] = array[1][2];
-	arrayAux[2][1] = array[2][2];
 	
-	arrayAux[0][0] = array[0][0] + array[0][1];
+	for(filas = 0; filas<maxFilas; filas++){
+		arrayAux[filas][1] = array[filas][2];
+	}
+/*	arrayAux[0][1] = array[0][2];
+	arrayAux[1][1] = array[1][2];
+	arrayAux[2][1] = array[2][2]; */
+	
+	//seccion 1
+	arrayAux[0][0] = array[0][0] + array[0][1];  
+	vector[0] = arrayAux[0][0] + arrayAux[0][1];
+	
+	
+	//seccion 2
 	arrayAux[1][0] = array[1][0] + array[1][1];
+	vector[1] = arrayAux[1][0] + arrayAux[1][1];
+	
+	//seccion 3
 	arrayAux[2][0] = array[2][0] + array[2][1];
+	vector[2] = arrayAux[2][0] + arrayAux[2][1];
 	
 	for(filas = 0; filas<3; filas++){
 		for(columnas = 0; columnas<2; columnas++){
@@ -32,15 +45,9 @@ int main(){
 		}
 	}
 	
-	vector[0] = arrayAux[0][0] + arrayAux[0][1];
-	vector[1] = arrayAux[1][0] + arrayAux[1][1];
-	vector[2] = arrayAux[2][0] + arrayAux[2][1];
 	
 	for(filas = 0; filas<3; filas++){
 		cout<<vector[filas];
 	}
-	
-	
-	
 	
 }
